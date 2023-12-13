51d1f1186403211baf915505e20950064538f7a5
Andrey Somov <public.somov@gmail.com>
2023-12-13 14:43:17 +0400

    Create project

---
+++ src/main/kotlin/show/dnddetector/DndDetectorApplication.kt
@@ -0,0 +1,11 @@
+package show.dnddetector
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DndDetectorApplication
+
+fun main(args: Array<String>) {
+	runApplication<DndDetectorApplication>(*args)
+}
---
+++ src/test/kotlin/show/dnddetector/DndDetectorApplicationTests.kt
@@ -0,0 +1,13 @@
+package show.dnddetector
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DndDetectorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
